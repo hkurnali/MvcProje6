cc3656eee7648a1a834e1789151e72639bd25abd
ADMIN <ADMIN@DESKTOP-JEPLJDD>
2023-08-11 23:40:01 +0300

    Add project files.

---
+++ DataAccessLayer/Concrate/Repositories/GenericRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DataAccessLayer.Concrate.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        
+        public void Delete(T p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> List(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
